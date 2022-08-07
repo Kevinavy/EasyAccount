e49f53c2eaddec44a8b26e65ea73b165af347792
王凯文 <kevinavy@ha.local>
2022-08-07 15:52:32 +0800

    Initial commit

---
+++ src/main/java/com/kevinavy/easyaccount/EasyAccountApplication.java
@@ -0,0 +1,13 @@
+package com.kevinavy.easyaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyAccountApplication.class, args);
+    }
+
+}
